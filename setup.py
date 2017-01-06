4253534d5a8c5eabf95627c8a2e5052382041988
Ash Wilson <ash.d.wilson@gmail.com>
2017-01-05 22:42:24 -0800

    chg: usr: Corrected version in setup.py !minor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(file_name):
     return filestring
 
 setup(name = "kalibrate",
-      version = "1.2",
+      version = "1.1.1",
       author = "Ash Wilson",
       author_email = "ash.d.wilson@gmail.com",
       description = "A python wrapper for kalibrate-rtl",
